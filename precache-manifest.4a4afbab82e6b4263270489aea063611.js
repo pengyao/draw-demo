4ba09eb9ef119d848be27db3b7165382fe7eac10
pengyao <pengyao@pengyao.org>
2019-05-05 15:40:25 +0800

    gh-pages

---
+++ precache-manifest.4a4afbab82e6b4263270489aea063611.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46676e6ee06b82c2df058acd0b7a3438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1212410eaf1bdeb1c6",
+    "url": "/static/css/main.64ae7e14.chunk.css"
+  },
+  {
+    "revision": "12b86522828d3d132b82",
+    "url": "/static/js/2.96efc01c.chunk.js"
+  },
+  {
+    "revision": "ac1212410eaf1bdeb1c6",
+    "url": "/static/js/main.3136f9a5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
